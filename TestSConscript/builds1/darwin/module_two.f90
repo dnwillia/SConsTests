0c39fd29701c3a12b71a061990965a6accd2bd6d
Dan Williams <dnwillia@gmail.com>
2023-06-28 00:37:39 -0400

    Remove the build files

--- TestSConscript/builds1/darwin/module_two.f90
+++
@@ -1,9 +0,0 @@
-module module_two
-  implicit none
-  private
-  public :: sayHelloTwo
-contains
-  subroutine sayHelloTwo()
-    print *,"Hello from module two!"
-  end subroutine sayHelloTwo
-end module module_two
